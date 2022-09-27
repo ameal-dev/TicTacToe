503faa024ae4cdb435c2c04f8cb3e5acbd20aacd
Emil Rydén <emiryd@gmail.com>
2022-09-27 10:09:35 +0200

    refactored isGameOver

---
+++ utils/isGameOver.tsx
@@ -0,0 +1,45 @@
+export const isGameOver = (board: string[]) => {
+	let isGameOver = false;
+	//! Win conditions
+	const firstRow = board[0] + board[1] + board[2];
+	const secondRow = board[3] + board[4] + board[5];
+	const thirdRow = board[6] + board[7] + board[8];
+	const firstCol = board[0] + board[3] + board[6];
+	const secondCol = board[1] + board[4] + board[7];
+	const thirdCol = board[2] + board[5] + board[8];
+	const topLeftBottomRight = board[0] + board[4] + board[8];
+	const topRightBottomLeft = board[2] + board[4] + board[6];
+
+	const winConArry = [];
+	winConArry.push(
+		firstRow,
+		secondRow,
+		thirdRow,
+		firstCol,
+		secondCol,
+		thirdCol,
+		topLeftBottomRight,
+		topRightBottomLeft
+	);
+
+	const winConversion = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	];
+
+	let winningSquares;
+	winConArry.forEach((row, idx) => {
+		if (row === "XXX" || row === "OOO") {
+			isGameOver = true;
+			winningSquares = winConversion[idx];
+		}
+	});
+
+	return { isGameOver, winningSquares };
+};
